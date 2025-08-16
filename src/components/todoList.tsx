a566de13343bfaf387e524cbabcec6fb228bbe84
Verdysh Volodymyr <verdyshvladimir@gmail.com>
2025-08-16 11:22:56 +0300

    fixed linter

--- src/components/todoList.tsx
+++ src/components/todoList.tsx
@@ -1,3 +1,6 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
+/* eslint-disable jsx-a11y/control-has-associated-label */
+
 import { useState } from 'react';
 import { Todo } from '../types/Todo';
 import cn from 'classnames';
